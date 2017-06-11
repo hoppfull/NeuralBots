34fb50c4b3ff8e3035f178d8fd89e8e9305de8c7
bjorn kihlberg <hoppfull@gmail.com>
2017-06-11 12:28:28 +0200

    add test file for search tree algorithm

---
+++ FSharpApp/Tests/Algorithms.SearchTreeTests.fs
@@ -0,0 +1,13 @@
+﻿namespace Algorithms
+
+module SearchTreeTests =
+    open System
+    open Xunit
+    open FsCheck
+    open FsCheck.Xunit
+
+    open Algorithms.SearchTree
+
+    [<Property>]
+    let ``createTree with no data returns an empty node`` maxDepth maxChildren =
+        Assert.Equal(createTree maxDepth maxChildren [], Empty)
